e819d27a92d3c1e585226ec099f84d848b6b42a1
Christian Jacobsen <christian@chjacobsen.se>
2016-03-31 15:23:24 +0200

    Added vimrc to the project

--- setupstuff.sh
+++ setupstuff.sh
@@ -2,6 +2,10 @@
 
 echo "Setting up things"
 echo "---"
+
+echo "Moving vimrc"
+cp generalvimrc ~/.vimrc
+
 echo "Getting pathogen"
 ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -10,8 +14,3 @@ echo "Getting youcompleteme"
 cd ~/.vim/bundle
 git clone https://github.com/valloric/youcompleteme
 
-echo "Grabbing vimrc"
-cd /tmp
-git clone https://gist.github.com/chjacobsen/d7a8a34656c8dd0fea4419a6bb5d2fbe
-cd d7a8a34656c8dd0fea4419a6bb5d2fbe
-cp generalvimrc ~/.vimrc
