ae2967bb9e1068d409c676b82bfa386dced77456
Christian Jacobsen <christian@chjacobsen.se>
2016-04-01 09:21:21 +0200

    Some package installation

--- setupstuff.sh
+++ setupstuff.sh
@@ -2,15 +2,17 @@
 
 echo "Setting up things"
 echo "---"
+sudo apt-get install vim
+sudo apt-get install curl
+sudo apt-get install python-pip
 
 echo "Moving vimrc"
 cp generalvimrc ~/.vimrc
 
-echo "Getting pathogen"
-~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-echo "Getting youcompleteme"
-cd ~/.vim/bundle
-git clone https://github.com/valloric/youcompleteme
-
+#echo "Getting pathogen"
+#~/.vim/autoload ~/.vim/bundle
+#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#
+#echo "Getting youcompleteme"
+#cd ~/.vim/bundle
+#git clone https://github.com/valloric/youcompleteme
